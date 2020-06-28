0bd677f6280adcdccc4f216dd6c3164a7cabac56
Kebigon <git@kebigon.xyz>
2020-06-28 18:50:47 +0900

    Use alacritty as default terminal

--- config.h
+++ config.h
@@ -30,7 +30,7 @@
  *
  * Can be set from command line with "-t program".
  */
-#define TERMINAL "urxvt"
+#define TERMINAL "alacritty"
 
 /*
  * Do we allow windows to be iconified? Set to true if you want this
